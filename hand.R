d7529bf2cff4c3fc6beadcfcd12322067f2593cc
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-09-07 14:55:54 -0500

    strain paths into multiple strain tables

--- hand.R
+++ hand.R
@@ -1,48 +1,73 @@
 #!/usr/bin/env Rscript
 
 library(tidyverse)
-# library(furrr) future_map
 
 # emacs text expansion
 # TODO: expand M-- to <- and C-M-m to %>%
 
-# So the approach is the following is:
-
-# Input: target folders with strain name
-# Output: plot of number of proteins
-
-TARGETS <- list(ecoli="/run/media/ebecerra/manjaro500/home/ebecerra/2-projects/results/e_coli_255/2-pfams")
+STRAINS <- list(ecoli="./test_strains/ecoli", cenocepacia="./test_strains/cenocepacia")
 
 
-HEADER <- c("protein",
-            "md5",
-            "lenght",
-            "analysis",
-            "memberDB",
-            "memberDB_txt",
-            "start",
-            "end",
-            "score",
-            "status",
-            "date",
-            "interpro",
-            "interpro_txt",
-            "GOs",
-            "pathways")
-
-
-read_strain_dirs <- function  (list_of_dirs)
+read_pfam_tsv <- function(pfam_tsv, strain)
 {
 
-  expand_into_tables <- function (path)
-  {
-    list.files(path, full.names = TRUE) %>%
-      str_subset("\\.tsv$") %>%
-      map(read_tsv, col_names = HEADER)
-  }
+  HEADER <- c("protein",
+              "md5",
+              "lenght",
+              "analysis",
+              "memberDB",
+              "memberDB_txt",
+              "start",
+              "end",
+              "score",
+              "status",
+              "date",
+              "interpro",
+              "interpro_txt",
+              "GOs",
+              "pathways")
 
-  map(list_of_dirs, expand_into_tables)
+  SUBSET <- c("protein",
+              "memberDB",
+              "memberDB_txt")
+
+  # regex is not robust, path needs at least one /
+  genome <- str_replace(pfam_tsv, ".*/(.*?)\\.pfam\\.tsv$", "\\1")
+
+  read_tsv(pfam_tsv, col_names = HEADER) %>%
+    select(all_of(SUBSET)) %>%
+    add_column(strain=strain, .before="protein") %>%
+    add_column(genome=genome, .before="strain")
 
 }
 
-x <- suppressMessages(read_strain_dirs(TARGETS))
+
+get_pfam_tsvs_from_strain_dir <- function(strain_dir)
+{
+    list.files(strain_dir, full.names = TRUE) %>%
+      str_subset("\\.tsv$")
+}
+
+TSVS <- get_pfam_tsvs_from_strain_dir(STRAINS[["ecoli"]])
+
+# main --------------------------------------------------------------------
+
+main <- function()
+{
+  strain_tables <- list()
+
+  for(strain in names(STRAINS))
+  {
+
+    pfam_tsvs <- get_pfam_tsvs_from_strain_dir(STRAINS[[strain]])
+    print(pfam_tsvs)
+    strain_tables[[strain]] <- map(pfam_tsvs, read_pfam_tsv, strain = strain)
+  }
+
+  return(strain_tables)
+
+}
+
+
+STRAIN_TABLES <- main()
+print(STRAIN_TABLES)

e45d8e8ce17fe530f847dcaf34bd13dc7d7fba0b
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-09-08 16:12:24 -0500

    wip: broken plotting

--- plotting.R
+++ plotting.R
@@ -1,1 +1,22 @@
 #!/usr/bin/env Rscript
+
+library(tidyverse)
+
+PAAR_ID <- "PF05593"
+RHS_ID <- "PF05488"
+  
+hits <- read_tsv("hits_per_genome.tsv", col_names = TRUE)
+
+paar <- hits |> filter(pfam == PAAR_ID)
+rhs <- hits |> filter(pfam == RHS_ID)
+
+paar |> 
+  add_count(protein)
+
+
+paar |> 
+  ggplot()+
+  geom_jitter(aes(x=strain, y=n))
+
+
+View(hits)

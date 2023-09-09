cde83f8641542785c911d7617e3778e6ef4ba6ad
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-09-08 20:32:20 -0500

    style

--- hits_per_genome.R
+++ hits_per_genome.R
@@ -7,68 +7,65 @@ library(tidyverse)
 # output: single table with
 # colnames: genome strain pfam counts matching_proteins
 
-STRAINS <- list(ecoli="./test_strains/ecoli", cenocepacia="./test_strains/cenocepacia")
+STRAINS <- list(ecoli = "./test_strains/ecoli", cenocepacia = "./test_strains/cenocepacia")
 
 SUBSET_PFAMS <- c("PF05593", "PF05488")
 
 
-read_pfam_tsv <- function(pfam_tsv, strain)
-{
+read_pfam_tsv <- function(pfam_tsv, strain) {
+  HEADER <- c(
+    "protein",
+    "md5",
+    "lenght",
+    "analysis",
+    "pfam",
+    "pfam_txt",
+    "start",
+    "end",
+    "score",
+    "status",
+    "date",
+    "interpro",
+    "interpro_txt",
+    "GOs",
+    "pathways"
+  )
 
-  HEADER <- c("protein",
-              "md5",
-              "lenght",
-              "analysis",
-              "pfam",
-              "pfam_txt",
-              "start",
-              "end",
-              "score",
-              "status",
-              "date",
-              "interpro",
-              "interpro_txt",
-              "GOs",
-              "pathways")
-
-  SUBSET_COLS <- c("protein",
-              "pfam",
-              "pfam_txt",
-              "start",
-              "end")
+  SUBSET_COLS <- c(
+    "protein",
+    "pfam",
+    "pfam_txt",
+    "start",
+    "end"
+  )
 
   # regex is not robust, path needs at least one /
   genome <- str_replace(pfam_tsv, ".*/(.*?)\\.pfam\\.tsv$", "\\1")
 
   return(
-  read_tsv(pfam_tsv, col_names = HEADER) %>%
-    select(all_of(SUBSET_COLS)) %>%
-    filter(pfam %in% SUBSET_PFAMS) %>%
-    add_column(strain=strain, .before="protein") %>%
-    add_column(genome=genome, .before="strain")
+    read_tsv(pfam_tsv, col_names = HEADER) %>%
+      select(all_of(SUBSET_COLS)) %>%
+      filter(pfam %in% SUBSET_PFAMS) %>%
+      add_column(strain = strain, .before = "protein") %>%
+      add_column(genome = genome, .before = "strain")
   )
-
 }
 
 
-get_pfam_tsvs_from_strain_dir <- function(strain_dir)
-{
-
+get_pfam_tsvs_from_strain_dir <- function(strain_dir) {
   return(
-  list.files(strain_dir, full.names = TRUE) %>%
-    str_subset("\\.tsv$")
+    list.files(strain_dir, full.names = TRUE) %>%
+      str_subset("\\.tsv$")
   )
 }
 
 # main --------------------------------------------------------------------
 
-get_strain_tables <- function()
-{
+get_strain_tables <- function() {
   strain_tables <- list()
 
-  for(strain in names(STRAINS))
+  for (strain in names(STRAINS))
   {
-
     pfam_tsvs <- get_pfam_tsvs_from_strain_dir(STRAINS[[strain]])
 
     multiple_tables_per_strain <- map(pfam_tsvs, read_pfam_tsv, strain = strain)
@@ -79,7 +76,6 @@ get_strain_tables <- function()
   }
 
   return(strain_tables)
-
 }
 
 
--- plotting.R
+++ plotting.R
@@ -4,19 +4,19 @@ library(tidyverse)
 
 PAAR_ID <- "PF05593"
 RHS_ID <- "PF05488"
-  
+
 hits <- read_tsv("hits_per_genome.tsv", col_names = TRUE)
 
 paar <- hits |> filter(pfam == PAAR_ID)
 rhs <- hits |> filter(pfam == RHS_ID)
 
-paar |> 
+paar |>
   add_count(protein)
 
 
-paar |> 
-  ggplot()+
-  geom_jitter(aes(x=strain, y=n))
+paar |>
+  ggplot() +
+  geom_jitter(aes(x = strain, y = n))
 
 
 View(hits)

193d40d71192d402998496f364773cc061f883f7
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-09-11 19:01:57 -0500

    plots ready

--- plotting.R
+++ plotting.R
@@ -8,11 +8,15 @@ RHS_ID <- "PF05488"
 hits <- read_tsv("hits_per_genome.tsv", col_names = TRUE)
 
 # to protein table
-x |> select(-start, -end) |> distinct() -> y
+hits |> select(-start, -end) |> distinct() -> y
 # collapse genomes and pfams
 y |> add_count(genome, pfam) |> select(-protein) |> distinct() -> z
 
 
 z |>
     ggplot(aes(x = strain, y = n))+
-    geom_boxplot()+geom_jitter(alpha=1/2, size=0.8, shape=1)+facet_grid(~pfam_txt)
+    geom_boxplot()+geom_jitter(alpha=1/2, size=0.8, shape=1)+facet_grid(~pfam_txt) -> p
+
+
+ggsave("paar_rhs.svg", p, width = 18, height = 12, units = "cm")
+ggsave("paar_rhs.png", p, width = 18, height = 12, units = "cm")
